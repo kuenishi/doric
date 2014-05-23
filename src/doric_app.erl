b6f67af156dae4e0d2b76d6d4edd7cb20b3e035f
UENISHI Kota <kota@basho.com>
2014-05-23 21:01:12 +0900

    basic import

---
+++ src/doric_app.erl
@@ -0,0 +1,16 @@
+-module(doric_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    doric_sup:start_link().
+
+stop(_State) ->
+    ok.
